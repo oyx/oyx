2ab98b2e8a2420a81a9dd9fc8c7f6379f54f5624
oyx <ouyangxue1993@hotmail.com>
2011-05-04 16:00:18 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oyx::Application.config.secret_token = 'ceae67b4b08190e33d8934d4b7eb4b4ed86401c95cf8b879883914710d9fa0388ac7b01e4e01ded1123470a7481bfd6ee20e207b2b368a4b68479144667a885b'
